7eaa7fb6b2b78b47a584505a46c3133c814078c0
Mythical Labs <mythicallabsremote@gmail.com>
2026-02-10 19:37:23 -0600

    V1.0 [BETA] Web Deployment

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const path = require('path')
 
 app.get('/', (req,res) => {
     res.set('Content-Type', 'text/html');
-    res.send(fs.readFileSync('./game/gui/pages/index.html'))
+    res.send(fs.readFileSync(path.join(process.cwd(), './game/gui/pages/index.html')))
 });
 app.get('/scratchembed', (req,res) => {
     const queryval = req.query.q;
     res.set('Content-Type', 'text/html');
-    res.send(fs.readFileSync(`./game/gui/pages/embed${queryval}.html`))
+    res.send(fs.readFileSync(path.join(process.cwd(), `./game/gui/pages/embed${queryval}.html`)))
 });
 app.get('/script', (req,res) => {
     res.set('Content-Type', 'application/javascript');
-    res.send(fs.readFileSync('./game/scripts/main.js'))
+    res.send(fs.readFileSync(path.join(process.cwd(), './game/scripts/main.js')))
 });
 app.get('/style', (req,res) => {
     res.set('Content-Type', 'text/css');
-    res.send(fs.readFileSync('./game/scripts/style.css'))
+    res.send(fs.readFileSync(path.join(process.cwd(), './game/scripts/style.css')))
 });
 app.listen(80);
